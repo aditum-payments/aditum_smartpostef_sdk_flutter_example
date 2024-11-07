a664da93dc722ea96e79210e6532390f8ba9b8fe
Lucas Trindade <lucas.trindade@aditum.com.br>
2024-11-07 18:06:28 -0300

    Add InitScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:aditum_smartpostef_sdk_flutter_example/screens/init_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,77 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: InitScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  static const platform = MethodChannel('br.com.aditum.payment'); // Nome do canal
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Aditum SDK Android'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                _invokeMethod('init');
-              },
-              child: const Text('Init'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                _invokeMethod('pay');
-              },
-              child: const Text('Pay'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                _invokeMethod('confirm');
-              },
-              child: const Text('Confirm'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                _invokeMethod('cancelation');
-              },
-              child: const Text('Cancelation'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                _invokeMethod('print');
-              },
-              child: const Text('Print'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Future<void> _invokeMethod(String method) async {
-    try {
-      final String result = await platform.invokeMethod(method);
-      print('Result: $result');
-    } on PlatformException catch (e) {
-      print('Failed to invoke: ${e.message}');
-    }
   }
 }
