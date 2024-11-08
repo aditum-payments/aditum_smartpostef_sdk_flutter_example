c7a2a3fab643b50523a8c7ec8e92196337e5fcdb
Lucas Trindade <lucas.trindade@aditum.com.br>
2024-11-08 03:17:47 -0300

    Change app name

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MethodChannel Example',
+      title: 'Aditum Flutter Example',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
